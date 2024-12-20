c3c1566c7606b5bdcbcb27456b193ba3dace0e36
Diniskaa <vladian65@gmail.com>
2024-12-20 21:41:19 +0300

    Update task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -17,6 +17,7 @@ class Book:
     """Класс для представления книги."""
 
     def __init__(self, id_: int, name: str, pages: int):
+        # Инициализируем объект книги с id, названием и количеством страниц
         self.id = id_
         self.name = name
         self.pages = pages
@@ -38,14 +39,17 @@ class Library:
 
     def get_next_book_id(self):
         """Возвращает следующий идентификатор для добавления новой книги."""
-        if not self.books:
-            return 1  # Если книг нет, следующий id будет 1
-        else:
-            # Идентификатор следующей книги - это id последней книги + 1
-            return self.books[-1].id + 1
+        # Собираем множество существующих идентификаторов книг
+        existing_ids = {book.id for book in self.books}
+        next_id = 1
+        # Ищем минимальный доступный идентификатор
+        while next_id in existing_ids:
+            next_id += 1
+        return next_id
 
     def get_index_by_book_id(self, book_id):
         """Возвращает индекс книги по ее id."""
+         # Проходим по всем книгам и ищем книгу с заданным идентификатором
         for index, book in enumerate(self.books):
             if book.id == book_id:
                 return index
@@ -53,13 +57,17 @@ class Library:
         raise ValueError("Книги с запрашиваемым id не существует")
 
 if __name__ == '__main__':
+     # Создаем пустую библиотеку для тестирования
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
 
+     # Создаем список книг из базы данных
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
+    # Инициализируем библиотеку с книгами
     library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
+    # Проверяем получение индекса книги по ее id
     print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1

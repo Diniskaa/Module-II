4f979e8b0889865a361ea11e0c42d3648d1319e2
Diniskaa <vladian65@gmail.com>
2024-12-20 21:34:11 +0300

    Update task_1.py

--- Лабораторная работа 2/task_1.py
+++ Лабораторная работа 2/task_1.py
@@ -13,11 +13,11 @@ BOOKS_DATABASE = [
 # TODO: написать класс Book
 class Book:
     """Класс для представления книги."""
-
+ # Инициализация атрибутов объекта Book
     def __init__(self, id_: int, name: str, pages: int):
-        self.id = id_
-        self.name = name
-        self.pages = pages
+        self.id = id_ # Уникальный id книги
+        self.name = name # Название книги
+        self.pages = pages # Количество страниц
 
     def __str__(self):
         """Возвращает строковое представление книги."""
@@ -28,11 +28,13 @@ class Book:
         return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
 
 if __name__ == '__main__':
-    # инициализируем список книг
+    # Инициализируем список книг на основе данных из BOOKS_DATABASE
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
+# Проверяем метод __str__ для каждого объекта в списке
     for book in list_books:
-        print(book)  # проверяем метод __str__
-
-    print(list_books)  # проверяем метод __repr__
+        print(book)  # проверяем метод __str__    # Должен вывести название
+        
+ # Проверяем метод __repr__, выводя весь список книг
+    print(list_books)  # проверяем метод __repr__    # Должен вывести строки, с помощью которых можно создать такие же объекты
